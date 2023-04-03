e1bb2f645458f3c17e52bd373433257934c44449
JL <julii.luna@live.com>
2023-04-03 16:31:10 -0300

    terminado

---
+++ README.md
@@ -0,0 +1,7 @@
+# Aplicación de clima (GeoLocation + OpenWeatherMaps)
+
+Esta es una Consola Interactiva para revisar el Clima con NodeJs + GeoLocation + OpenWeatherMaps
+
+* Ejecutar npm install para instalar las dependencias.
+* Configurar las APIs de Position Stack y Open Weather Maps en el archivo .env.
+* Ejecutar node index para correr la aplicación.
---
+++ index.js
@@ -0,0 +1,69 @@
+require('dotenv').config()
+
+const { leerInput, inquirerMenu, pausa, listarLugares } = require('./helpers/inquirer');
+const Busquedas = require('./models/busquedas');
+
+
+const main = async() => {
+    
+    const busquedas = new Busquedas();
+    let opt;
+
+    do{
+
+        opt = await inquirerMenu();
+
+        switch ( opt ) {
+
+            case 1:
+                // Mostrar mensaje para que la persona escriba
+                const termino = await leerInput('Ciudad: ');
+
+                // Buscar los lugares
+                const lugares = await busquedas.ciudad( termino );
+
+                // Seleccionar el lugar
+                const id = await listarLugares(lugares);
+                /* console.log({id}) */
+                if ( id==='0') continue;
+
+                const lugarSel = lugares.find( l => l.id == id);
+                /* console.log("lugarsel", lugarSel); */
+
+                // Guardar en DB
+                busquedas.agregarHistorial( lugarSel.nombre );
+
+
+                // Datos del clima
+                const clima = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng)
+                //console.log(clima);
+
+                // Mostrar resultados
+                console.clear();
+                console.log('\nInformacion de la ciudad\n'.green);
+                console.log('Ciudad:', lugarSel.nombre.green );
+                console.log('Lat:', lugarSel.lat );
+                console.log('Lng:', lugarSel.lng );
+                console.log('Temperatura:', clima.temp);
+                console.log('Minima:', clima.min);
+                console.log('Maxima:', clima.max);
+                console.log('Descripcion:', clima.desc);
+
+            break;
+
+            case 2:
+                busquedas.historialCapitalizado.forEach( (lugar, i) => {
+                    const idx = `${ i + 1 }.`.green;
+                    console.log( `${ idx } ${ lugar } ` );
+                })
+
+            
+            break;
+        }
+
+        if ( opt !== 0 ) await pausa();
+
+    } while (opt !== 0 )
+}
+
+main();
